570febd791861509a835b8f49727149e86cb7e4e
teju12b5 <tejaswini.uppugunduri@gmail.com>
2024-09-08 15:01:29 -0400

    commit

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -11,10 +11,6 @@ public class TestController {
     public String getData() {
         return "First message from AWS Ecs";
     }
-    @GetMapping("/message")
-    public String getMessage() {
-        return "third message from AWS Ecs";
-    }
-
+    
     
 }
